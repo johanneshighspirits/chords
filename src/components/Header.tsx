42b22fecb470de578efd425c7a141ed04b74dd82
Johannes Borgström <johannes.borgstrom@1337.tech>
2024-04-11 22:20:48 +0200

    Google auth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
+import { SignIn } from './auth/SignIn';
 import { Container } from './layout/PageContainer';
 
 export const Header = () => (
   <header className={styles.Header}>
     <Container>
       <h1>Chords</h1>
+      <SignIn />
     </Container>
   </header>
 );
